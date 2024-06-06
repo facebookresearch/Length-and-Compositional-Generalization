5e7832ff7e9fb5aed3ce112055d732e230037023
amin <amansouri3476@gmail.com>
2024-06-06 09:41:39 -0400

    add headers

--- src/datamodules/set.py
+++ src/datamodules/set.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from .base import BaseStreamingDataset, BaseStreamingPLDataModule
 from typing import Optional, Callable, Dict
 from src.utils.datamodule import select_distribution
--- src/models/decoder_lightning.py
+++ src/models/decoder_lightning.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import os
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 import hydra
